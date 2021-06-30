2b299621d76367728a95fdfb737389c66bcdc951
jrooms <wanted0301@gmail.com>
2021-06-30 23:33:00 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kim Jeong Hun on 2021/06/30.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
