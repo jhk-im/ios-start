661baa9cfd68a113dffb82df53be3d87ce0ff452
jrooms <wanted0301@gmail.com>
2021-07-01 00:19:04 +0900

    ios start

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            MapView()
+                .frame(height: 500)
+                .ignoresSafeArea(edges: .bottom)
+                .frame(height: 500)
+            
+            CircleImage()
+                .offset(y: -100)
+                .padding(.bottom, -100)
+            
+            VStack(alignment: .leading) {
+                Text("Vertical Title")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+                HStack {
+                    Text("Title")
+                        .font(.subheadline)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    Text("Sub Title")
+                        .font(.subheadline)
+                        .foregroundColor(.red)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text goes here.")
+            }.padding()
+            
+            Spacer()
+        }
     }
 }
 
