e8b6e2030b731899dbc58c7d423e311f1a88c1a5
jrooms <wanted0301@gmail.com>
2021-07-08 01:48:45 +0900

    iOS start

---
+++ Landmarks/Views/RptatedBadgeSu,bp;.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RptatedBadgeSu_bp_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RptatedBadgeSu_bp__Previews: PreviewProvider {
+    static var previews: some View {
+        RptatedBadgeSu_bp_()
+    }
+}
